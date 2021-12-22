904a2c687c734ad6d34113741294dd2d69cb37e8
Sumiya-appier <sumiya.ushiro@appier.com>
2021-12-22 15:35:06 +0900

    Adjust style

--- src/components/templates/Blog.tsx
+++ src/components/templates/Blog.tsx
@@ -2,46 +2,100 @@
 // /** @jsx jsx */
 import { VFC } from 'react';
 import { jsx, css } from '@emotion/react';
+import { Container, Grid } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const headerContent = css`
+const container = css`
+  margin: 50px;
   width: 100%;
-  max-width: 1600px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  @media (max-width: 680px) {
-    flex-direction: column;
-  }
+`;
+const header = css`
+  margin-bottom: 30px;
+`;
+const card = css`
+  padding: 30px 15px !important;
+`;
+const images = css`
+  margin: 40px 0 !important;
+  display: flex !important;
+  align-items: center !important;
 `;
 
 const image = css`
-  width: 50%;
-  @media (max-width: 680px) {
-    flex-direction: column;
-  }
+  max-width: 50%;
+  height: 80px;
+  background-color: #2fb3ff;
+  border-radius: 10%;
+  padding: 5px;
+`;
+const title = css`
+  margin-top: 10px;
+  margin-bottom: 0 !important;
+`;
+
+const caption = css`
+  margin-top: 5px;
 `;
 
 const Blogs: VFC = () => (
-  <div css={headerContent}>
-    <h1>Blogs</h1>
-    <div>
-      <img
-        src="https://hashcloak.com/graphics/noun_Science_3306446.png"
-        alt="research"
-        css={image}
-      />
-      <h2>Research</h2>
-    </div>
-    <div>
-      <img
-        src="https://hashcloak.com/graphics/noun_Announcement_3166042.png"
-        alt="announcement"
-        css={image}
-      />
-      <h2>Announcement</h2>
-    </div>
-  </div>
+  <Container css={container}>
+    <Container text textAlign="center" css={header}>
+      <h2>Clients and Research Supporters</h2>
+      <Grid columns={2} stackable css={images} celled="internally">
+        <Grid.Row>
+          <Grid.Column css={card}>
+            <Link to="/">
+              <img
+                src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-research-online-learning-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"
+                alt="research"
+                css={image}
+              />
+              <h3 css={title}>Research</h3>
+            </Link>
+          </Grid.Column>
+          <Grid.Column css={card}>
+            <Link to="/">
+              <img
+                src="https://img.icons8.com/ios/50/000000/about.png"
+                alt="announcement"
+                css={image}
+              />
+              <h3 css={title}>Announcement</h3>
+            </Link>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+        <Grid.Column css={card}>
+            <Link to="/">
+              <img
+                src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-news-news-kiranshastry-lineal-kiranshastry-1.png"
+                alt="newsletter"
+                css={image}
+              />
+              <h3 css={title}>HashCloak Newsletter</h3>
+              <p css={caption}>
+                Periodic updates about current HashCloak projects and offerings
+              </p>
+            </Link>
+          </Grid.Column>
+          <Grid.Column css={card}>
+            <Link to="/">
+              <img
+                src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-news-news-kiranshastry-solid-kiranshastry-1.png"
+                alt="newsletter"
+                css={image}
+              />
+              <h3 css={title}>ZKMesh Newsletter</h3>
+              <p css={caption}>
+                Monthly Newsletter on privacy-preserving tech with Anna Rose of
+                the ZKPodcast
+              </p>
+            </Link>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  </Container>
 );
 
 export default Blogs;
