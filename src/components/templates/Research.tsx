f2a8450734e447d4e4d38eb7d044b64e845e5f86
Sumiya-appier <sumiya.ushiro@appier.com>
2021-12-22 13:03:19 +0900

    Adjust style

--- src/components/templates/Research.tsx
+++ src/components/templates/Research.tsx
@@ -1,67 +1,75 @@
 // /** @jsxRuntime classic */
 // /** @jsx jsx */
 import React, { VFC } from 'react';
-import { jsx, css } from '@emotion/react';
 import { Link } from 'react-router-dom';
+import { Container, Grid } from 'semantic-ui-react';
 
-const headerContent = css`
+import { jsx, css } from '@emotion/react';
+
+const container = css`
+  margin: 50px;
   width: 100%;
-  max-width: 1600px;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  @media (max-width: 680px) {
-    flex-direction: column;
-  }
 `;
-const blog = css`
-  display: flex;
-  flex-direction: column;
-  @media (max-width: 680px) {
-    flex-direction: column;
-  }
+const header = css`
+  margin-bottom: 30px;
+`;
+const caption = css`
+  line-height: 0.4rem;
 `;
 
 const Research: VFC = () => (
-  <>
-    <div>
-      <h1>Research</h1>
-      <p>
-        Here is a taste of the research we conduct and open problems we are
-        currently tackling.
-      </p>
-    </div>
-    <div css={headerContent}>
-      <div css={blog}>
-        <h2>2021</h2>
-        <Link to="/">
-          An Empirical Analysis of Ethereum as a Randomness Beacon (Ongoing)
-        </Link>
-        <Link to="/">SoK on Universal SNARK constructions (Ongoing)</Link>
-        <Link to="/">
-          Katzenmint: A Decentralized PKI System for Anonymous Communication
-          Networks (Ongoing)
-        </Link>
-        <Link to="/">
-          Towards a Dynamic Parameterization Algorithm for the Loopix Anonymity
-          Systems (Ongoing)
-        </Link>
-        <h2>2020</h2>
-        <Link to="/">
-          Blockchain is Watching You: Profiling and Deanonymizing Ethereum Users
-        </Link>
-        <h2>2019</h2>
-        <Link to="/">
-          Short Paper: Towards Characterizing Sybil Attacks in Cryptocurrency
-          Mixers
-        </Link>
-      </div>
-      <div>
-        <h2>Open Research Problems </h2>
-        <Link to="/">TBA</Link>
-      </div>
-    </div>
-  </>
+  <Container css={container}>
+    <Container text textAlign="center" css={header}>
+      <h2>Research</h2>
+      <p css={caption}>Here is a taste of the research we conduct</p>
+      <p css={caption}>and open problems we are currently tackling.</p>
+    </Container>
+    <Grid columns={2} stackable centered column>
+      <Grid.Column>
+        <h3>2021</h3>
+        <p>
+          <Link to="/">
+            An Empirical Analysis of Ethereum as a Randomness Beacon (Ongoing)
+          </Link>
+        </p>
+        <p>
+          <Link to="/">SoK on Universal SNARK constructions (Ongoing)</Link>
+        </p>
+        <p>
+          <Link to="/">
+            Katzenmint: A Decentralized PKI System for Anonymous Communication
+            Networks (Ongoing)
+          </Link>
+        </p>
+        <p>
+          <Link to="/">
+            Towards a Dynamic Parameterization Algorithm for the Loopix
+            Anonymity Systems (Ongoing)
+          </Link>
+        </p>
+        <h3>2020</h3>
+        <p>
+          <Link to="/">
+            Blockchain is Watching You: Profiling and Deanonymizing Ethereum
+            Users
+          </Link>
+        </p>
+        <h3>2019</h3>
+        <p>
+          <Link to="/">
+            Short Paper: Towards Characterizing Sybil Attacks in Cryptocurrency
+            Mixers
+          </Link>
+        </p>
+      </Grid.Column>
+      <Grid.Column>
+        <h3>Open Research Problems </h3>
+        <p>
+          <Link to="/">TBA</Link>
+        </p>
+      </Grid.Column>
+    </Grid>
+  </Container>
 );
 
 export default Research;
--- src/components/templates/Services.tsx
+++ src/components/templates/Services.tsx
@@ -2,19 +2,19 @@
 // /** @jsx jsx */
 import { VFC } from 'react';
 import { jsx, css } from '@emotion/react';
+import { Container } from 'semantic-ui-react';
 
 const container = css`
   width: 100%;
-  max-width: 1600px;
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
   position: relative;
   overflow: hidden;
-  @media (max-width: 680px) {
+  /* @media (max-width: 680px) {
     flex-direction: column;
-  }
+  } */
 `;
 
 const services = css`
@@ -22,9 +22,9 @@ const services = css`
   margin: 100px 0;
   z-index: 1;
   background-color: transparent;
-  @media (max-width: 680px) {
+  /* @media (max-width: 680px) {
     flex-direction: column;
-  }
+  } */
 `;
 
 const image = css`
@@ -40,27 +40,29 @@ const image = css`
     black calc(100% - 1.5em),
     transparent
   );
-  @media (max-width: 680px) {
+  /* @media (max-width: 680px) {
     flex-direction: column;
-  }
+  } */
 `;
 
 const Services: VFC = () => (
-  <div css={container}>
-    <div css={services}>
-      <h2>Services</h2>
-      <p>Smart contract audits</p>
-      <p>Privacy audits</p>
-      <p>Cryptographic implementation audits</p>
-      <p>Bespoke R&D for blockchain-related projects</p>
-      <p>General Cryptography and Blockchain consulting</p>
+  <Container>
+    <div css={container}>
+      <div css={services}>
+        <h2>Services</h2>
+        <p>Smart contract audits</p>
+        <p>Privacy audits</p>
+        <p>Cryptographic implementation audits</p>
+        <p>Bespoke R&D for blockchain-related projects</p>
+        <p>General Cryptography and Blockchain consulting</p>
+      </div>
+      <img
+        src="https://hashcloak.com/graphics/hc-contract.png"
+        alt="service"
+        css={image}
+      />
     </div>
-    <img
-      src="https://hashcloak.com/graphics/hc-contract.png"
-      alt="service"
-      css={image}
-    />
-  </div>
+  </Container>
 );
 
 export default Services;
