4c9e7c86c5b6a5dcfc7a5e722007af1ee783f11f
sumiya_ushiro <u.sumi0820@gmail.com>
2021-12-23 01:04:47 +0900

    Add SNS links

--- src/components/templates/MainVisual.tsx
+++ src/components/templates/MainVisual.tsx
@@ -11,7 +11,7 @@ const mainVisual = css`
   flex-direction: row;
   align-items: center;
   position: relative;
-  @media (max-width: 980px) {
+  @media (max-width: 1200px) {
     margin-top: 70px;
     margin-bottom: 50px;
   }
@@ -57,11 +57,10 @@ const subTitle = css`
 `;
 
 const caption = css`
-  margin: 25px 0;
+  margin-top: 25px;
   line-height: 1.2em;
   @media (max-width: 770px) {
     margin-top: 20px;
-    margin-bottom: 30px;
     line-height: 1.2em;
     width: 80%;
     color: white;
@@ -93,6 +92,17 @@ const mbImage = css`
     background-blend-mode: lighten;
   }
 `;
+const icons = css`
+  display: flex;
+  }
+`;
+const sns = css`
+  margin: 0 20px;
+  width: 50px;
+  @media (max-width: 769px) {
+    width: 30px;
+  }
+`;
 
 const MainVisual: VFC = () => (
   <Container>
@@ -107,6 +117,30 @@ const MainVisual: VFC = () => (
           surrounding privacy-preserving technologies and blockchain
           infrastructure design.
         </p>
+        <div css={icons}>
+          <a
+            href="https://github.com/hashcloak"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png"
+              alt="github"
+              css={sns}
+            />
+          </a>
+          <a
+            href="https://twitter.com/hashcloak"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/ios-glyphs/60/ffffff/twitter.png"
+              alt="twitter"
+              css={sns}
+            />
+          </a>
+        </div>
       </div>
       <img
         src="https://hashcloak.com/graphics/hashcloak-ice.png"
@@ -126,6 +160,30 @@ const MainVisual: VFC = () => (
             surrounding privacy-preserving technologies and blockchain
             infrastructure design.
           </p>
+          <div css={icons}>
+            <a
+              href="https://github.com/hashcloak"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png"
+                alt="github"
+                css={sns}
+              />
+            </a>
+            <a
+              href="https://twitter.com/hashcloak"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://img.icons8.com/ios-glyphs/60/ffffff/twitter.png"
+                alt="twitter"
+                css={sns}
+              />
+            </a>
+          </div>
         </Grid.Column>
       </Grid>
     </Container>
